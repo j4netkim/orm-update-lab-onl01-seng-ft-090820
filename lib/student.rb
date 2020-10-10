d17bb8c8130081ea43b34719fd82de0cd0eb838e
J K <jkim65@buffalo.edu>
2020-10-10 21:37:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,13 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :id :name, :grade
+  attr_accessor :name, :grade
 
   def initialize(id=nil, name, grade)
     @id = id
     @name = name
     @grade = grade
-  end 
+  end
 
 
 end
