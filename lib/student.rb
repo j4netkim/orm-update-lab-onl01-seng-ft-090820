d6d1d82b1a5682886e2862c515b584fa4e50341c
J K <jkim65@buffalo.edu>
2020-10-10 21:46:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,8 +33,10 @@ class Student
 
   def save
     sql = <<-SQL
-    
+    INSERT INTO students (name, grade)
+    VALUES (?,?)
     SQL
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
 end
